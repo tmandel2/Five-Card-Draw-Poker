34677f0796206003b3842b2e62574bae1b7904a0
Tim Mandel <timothy.mandel@gmail.com>
2019-01-07 14:46:49 -0600

    Fold button updates wallets

--- js/app.js
+++ js/app.js
@@ -13,12 +13,19 @@ class player {
 		this.name = playerName,
 		this.wallet = 1500,
 		this.currentBet = 0,
-		this.currentCards = []
+		this.currentCards = [],
+		this.currentHandValue = 0
 	}
 	makeBet (amount) {
 		this.currentBet = this.currentBet + amount;
 		this.wallet = this.wallet - amount;
 		return this.currentBet
+	}
+	handEnd () {
+		this.currentBet = 0;
+	}
+	winHand (amount) {
+		this.wallet = this.wallet + amount;
 	}
 }
 
@@ -35,6 +42,7 @@ const game= {
 	whosTurn: null,
 	bettingRound: true,
 	drawingRound: false,
+	handNumber: 0,
 	startGame () {
 		this.player1 = new player($('#player1-name-input').val());
 		this.player2 = new player($('#player2-name-input').val());
@@ -46,6 +54,7 @@ const game= {
 		$('#card-location').css("visibility", "visible");
 		$('#player1').text(`${this.player1.name}`);
 		$('#player2').text(`${this.player2.name}`);
+		$('#player1').css('color', 'red');
 		$("#player1-stats").append(`<p>Wallet: ${this.player1.wallet}</br>Current Bet: ${this.player1.currentBet}</p>`);
 		$("#player2-stats").append(`<p>Wallet: ${this.player2.wallet}</br>Current Bet: ${this.player2.currentBet}</p>`);
 		this.whosTurn = 1;
@@ -58,25 +67,59 @@ const game= {
 	changeTurn () {
 		if (this.whosTurn === 1) {
 			this.whosTurn = 2;
+			$('#player1').css('color', 'white');
+			$('#player2').css('color', 'red');
 		} else if (this.whosTurn === 2) {
 			this.whosTurn = 1;
+			$('#player2').css('color', 'white');
+			$('#player1').css('color', 'red');
 		}
 	},
 	makeBet () {
 		let $betAmount = parseInt($('#bet-amount').val(), 10);
-		if (this.player1.currentBet - this.player2.currentBet > 0) {
-			$('#call').text(`Call ${this.player1.currentBet - this.player2.currentBet}`);
-		} else if (this.player1.currentBet - this.player2.currentBet < 0) {
-			$('#call').text(`Call ${this.player2.currentBet - this.player1.currentBet}`);
-		}
 		if (this.whosTurn === 1) {
 			this.player1.makeBet($betAmount);
 		} else if (this.whosTurn === 2) {
 			this.player2.makeBet($betAmount);
 		}
+		if (this.player1.currentBet - this.player2.currentBet > 0) {
+			$('#call').text(`Call ${this.player1.currentBet - this.player2.currentBet}`);
+		} else if (this.player1.currentBet - this.player2.currentBet < 0) {
+			$('#call').text(`Call ${this.player2.currentBet - this.player1.currentBet}`);
+		} else if (this.player1.currentBet - this.player2.currentBet === 0) {
+			$('#call').text(`Check`);
+		}
 		this.pot = this.player1.currentBet + this.player2.currentBet;
 		this.updateStats();
 		this.changeTurn();
+	},
+	makeCall () {
+		if (this.player1.currentBet > this.player2.currentBet) {
+			let $currentBet = this.player1.currentBet - this.player2.currentBet;
+			this.player2.makeBet($currentBet);
+		} else if (this.player1.currentBet < this.player2.currentBet) {
+			let $currentBet = this.player2.currentBet - this.player1.currentBet;
+			this.player1.makeBet($currentBet);
+		} else if (this.player1.currentBet === this.player2.currentBet) {
+			// NEED TO CHANGE TO THE HOLD CARD PHASE
+		}
+		this.pot = this.player1.currentBet + this.player2.currentBet;
+		this.updateStats();
+		this.changeTurn();
+	},
+	makeFold () {
+		if (this.whosTurn === 1) {
+			this.player2.winHand(this.pot);
+			this.pot = 0;
+			this.player2.handEnd();
+			this.player1.handEnd();
+		} else {
+			this.player1.winHand(this.pot);
+			this.pot = 0;
+			this.player2.handEnd();
+			this.player1.handEnd();
+		}
+		this.updateStats();
 	},
 	updateStats () {
 		$('#player1-stats p').html(`<p>Wallet: ${this.player1.wallet}</br>Current Bet: ${this.player1.currentBet}</p>`);
@@ -147,6 +190,16 @@ $('#bet-submit').on('click', (e) => {
 	e.preventDefault();
 	game.makeBet();
 });
+
+$('#call').on('click', (e) => {
+	e.preventDefault();
+	game.makeCall();
+});
+
+$('#fold').on('click', (e) => {
+	e.preventDefault();
+	game.makeFold();
+})
 
 $('img').on('click', (e) => {
 	game.holdCard(e.target);
