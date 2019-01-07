ae2e31420e9aacb7fdd77ed13a64487e5dc7c4b0
Tim Mandel <timothy.mandel@gmail.com>
2019-01-07 10:46:27 -0600

    Cards coded

---
+++ js/cards.js
@@ -0,0 +1,314 @@
+const cards = {
+	twoHearts: {
+		value: 2;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/2_of_hearts.png";
+		inPlay: null;
+	},
+	twoSpades: {
+		value: 2;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/2_of_spades.png";
+		inPlay: null;
+	},
+	twoDiamonds: {
+		value: 2;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/2_of_diamonds.png";
+		inPlay: null;
+	},
+	twoClubs: {
+		value: 2;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/2_of_clubs.png";
+		inPlay: null;
+	},
+	threeHearts: {
+		value: 3;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/3_of_hearts.png";
+		inPlay: null;
+	},
+	threeSpades: {
+		value: 3;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/3_of_spades.png";
+		inPlay: null;
+	},
+	threeDiamonds: {
+		value: 3;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/3_of_diamonds.png";
+		inPlay: null;
+	},
+	threeClubs: {
+		value: 3;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/3_of_clubs.png";
+		inPlay: null;
+	},
+	fourHearts: {
+		value: 4;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/4_of_hearts.png";
+		inPlay: null;
+	},
+	fourSpades: {
+		value: 4;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/4_of_spades.png";
+		inPlay: null;
+	},
+	fourDiamonds: {
+		value: 4;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/4_of_diamonds.png";
+		inPlay: null;
+	},
+	fourClubs: {
+		value: 4;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/4_of_clubs.png";
+		inPlay: null;
+	},
+	fiveHearts: {
+		value: 5;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/5_of_hearts.png";
+		inPlay: null;
+	},
+	fiveSpades: {
+		value: 5;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/5_of_spades.png";
+		inPlay: null;
+	},
+	fiveDiamonds: {
+		value: 5;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/5_of_diamonds.png";
+		inPlay: null;
+	},
+	fiveClubs: {
+		value: 5;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/5_of_clubs.png";
+		inPlay: null;
+	},
+	sixHearts: {
+		value: 6;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/6_of_hearts.png";
+		inPlay: null;
+	},
+	sixSpades: {
+		value: 6;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/6_of_spades.png";
+		inPlay: null;
+	},
+	sixDiamonds: {
+		value: 6;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/6_of_diamonds.png";
+		inPlay: null;
+	},
+	sixClubs: {
+		value: 6;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/6_of_clubs.png";
+		inPlay: null;
+	},
+	sevenHearts: {
+		value: 7;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/7_of_hearts.png";
+		inPlay: null;
+	},
+	sevenSpades: {
+		value: 7;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/7_of_spades.png";
+		inPlay: null;
+	},
+	sevenDiamonds: {
+		value: 7;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/7_of_diamonds.png";
+		inPlay: null;
+	},
+	sevenClubs: {
+		value: 7;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/7_of_clubs.png";
+		inPlay: null;
+	},
+	eightHearts: {
+		value: 8;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/8_of_hearts.png";
+		inPlay: null;
+	},
+	eightSpades: {
+		value: 8;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/8_of_spades.png";
+		inPlay: null;
+	},
+	eightDiamonds: {
+		value: 8;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/8_of_diamonds.png";
+		inPlay: null;
+	},
+	eightClubs: {
+		value: 8;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/8_of_clubs.png";
+		inPlay: null;
+	},
+	nineHearts: {
+		value: 9;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/9_of_hearts.png";
+		inPlay: null;
+	},
+	nineSpades: {
+		value: 9;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/9_of_spades.png";
+		inPlay: null;
+	},
+	nineDiamonds: {
+		value: 9;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/9_of_diamonds.png";
+		inPlay: null;
+	},
+	nineClubs: {
+		value: 9;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/9_of_clubs.png";
+		inPlay: null;
+	},
+	tenHearts: {
+		value: 10;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/10_of_hearts.png";
+		inPlay: null;
+	},
+	tenSpades: {
+		value: 10;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/10_of_spades.png";
+		inPlay: null;
+	},
+	tenDiamonds: {
+		value: 10;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/10_of_diamonds.png";
+		inPlay: null;
+	},
+	tenClubs: {
+		value: 10;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/10_of_clubs.png";
+		inPlay: null;
+	},
+	jackHearts: {
+		value: 11;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/jack_of_hearts.png";
+		inPlay: null;
+	},
+	jackSpades: {
+		value: 11;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/jack_of_spades.png";
+		inPlay: null;
+	},
+	jackDiamonds: {
+		value: 11;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/jack_of_diamonds.png";
+		inPlay: null;
+	},
+	jackClubs: {
+		value: 11;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/jack_of_clubs.png";
+		inPlay: null;
+	},
+	queenHearts: {
+		value: 12;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/queen_of_hearts.png";
+		inPlay: null;
+	},
+	queenSpades: {
+		value: 12;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/queen_of_spades.png";
+		inPlay: null;
+	},
+	queenDiamonds: {
+		value: 12;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/queen_of_diamonds.png";
+		inPlay: null;
+	},
+	queenClubs: {
+		value: 12;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/queen_of_clubs.png";
+		inPlay: null;
+	},
+	kingHearts: {
+		value: 13;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/king_of_hearts.png";
+		inPlay: null;
+	},
+	kingSpades: {
+		value: 13;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/king_of_spades.png";
+		inPlay: null;
+	},
+	kingDiamonds: {
+		value: 13;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/king_of_diamonds.png";
+		inPlay: null;
+	},
+	kingClubs: {
+		value: 13;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/king_of_clubs.png";
+		inPlay: null;
+	},
+	aceHearts: {
+		value: 14;
+		suit: "hearts";
+		image: "images/Playing_cards/playing-cards/ace_of_hearts.png";
+		inPlay: null;
+	},
+	aceSpades: {
+		value: 14;
+		suit: "spades";
+		image: "images/Playing_cards/playing-cards/ace_of_spades.png";
+		inPlay: null;
+	},
+	aceDiamonds: {
+		value: 14;
+		suit: "diamonds";
+		image: "images/Playing_cards/playing-cards/ace_of_diamonds.png";
+		inPlay: null;
+	},
+	aceClubs: {
+		value: 14;
+		suit: "clubs";
+		image: "images/Playing_cards/playing-cards/ace_of_clubs.png";
+		inPlay: null;
+	},
+}
